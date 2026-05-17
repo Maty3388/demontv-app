d3e798a183be19408a9a2970dc2e22ce9f9d8657
Maty3388 <Eithan100522@gmail.com>
2026-05-17 16:54:05 -0300

    fix: TV detection by landscape + aspect ratio

--- lib/utils/device_type.dart
+++ lib/utils/device_type.dart
@@ -5,7 +5,12 @@ enum DeviceType { phone, tv }
 DeviceType getDeviceType(BuildContext context) {
   final size = MediaQuery.of(context).size;
   final width = size.width;
-  // TV Box generalmente tiene 960dp+ de ancho
+  final height = size.height;
+  // Detectar TV por orientacion landscape y aspect ratio
+  final isLandscape = width > height;
+  final aspectRatio = width / height;
+  // TV boxes siempre en landscape con aspect ratio ~1.77 (16:9)
+  if (isLandscape && aspectRatio > 1.5) return DeviceType.tv;
   if (width >= 500) return DeviceType.tv;
   return DeviceType.phone;
 }
