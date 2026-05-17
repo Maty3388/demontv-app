fb59507ea31ef75467d3658898bce56aed52c2a0
Maty3388 <Eithan100522@gmail.com>
2026-05-17 17:27:49 -0300

    feat: force TV layout

--- lib/utils/device_type.dart
+++ lib/utils/device_type.dart
@@ -2,16 +2,10 @@ import "package:flutter/material.dart";
 
 enum DeviceType { phone, tv }
 
+const bool kIsTV = true;
+
 DeviceType getDeviceType(BuildContext context) {
-  final size = MediaQuery.of(context).size;
-  final width = size.width;
-  final height = size.height;
-  // Detectar TV por orientacion landscape y aspect ratio
-  final isLandscape = width > height;
-  final aspectRatio = width / height;
-  // TV boxes siempre en landscape con aspect ratio ~1.77 (16:9)
-  if (isLandscape && aspectRatio > 1.5) return DeviceType.tv;
-  if (width >= 500) return DeviceType.tv;
+  if (kIsTV) return DeviceType.tv;
   return DeviceType.phone;
 }
 
