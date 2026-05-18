f9b10b7f142d28b29e9f138e28b9fdb59798a52a
Maty3388 <Eithan100522@gmail.com>
2026-05-18 00:14:40 -0300

    fix: device_type for phone app

--- lib/utils/device_type.dart
+++ lib/utils/device_type.dart
@@ -2,10 +2,15 @@ import "package:flutter/material.dart";
 
 enum DeviceType { phone, tv }
 
-// TRUE = APK para Android TV/TV Box
-// FALSE = APK para celular
-const bool true = true;
+DeviceType getDeviceType(BuildContext context) {
+  final size = MediaQuery.of(context).size;
+  final width = size.width;
+  final height = size.height;
+  final isLandscape = width > height;
+  final aspectRatio = width / height;
+  if (isLandscape && aspectRatio > 1.5) return DeviceType.tv;
+  if (width >= 500) return DeviceType.tv;
+  return DeviceType.phone;
+}
 
-DeviceType getDeviceType(BuildContext context) => true ? DeviceType.tv : DeviceType.phone;
-
-bool isTV(BuildContext context) => true;
+bool isTV(BuildContext context) => getDeviceType(context) == DeviceType.tv;
