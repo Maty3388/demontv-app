602c60837e4be3d432da1ac8b7eff1c493ce5db8
Maty3388 <Eithan100522@gmail.com>
2026-05-30 10:54:55 -0300

    fix: player_screen use video_player only

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -1,6 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:media_kit/media_kit.dart';
-import 'package:media_kit_video/media_kit_video.dart';
+import 'package:video_player/video_player.dart';
 import '../models/models.dart';
 import '../theme/app_theme.dart';
 
@@ -11,16 +11,13 @@ class PlayerScreen extends StatefulWidget {
 }
 
 class _State extends State<PlayerScreen> {
-  late final Player _player;
-  late final VideoController _ctrl;
+  VideoPlayerController? _ctrl;
+  bool _buffering = true;
+  Timer? _reconnectTimer;
+  int _reconnectAttempts = 0;
 
   @override
-  void initState() {
-    super.initState();
-    _player = Player();
-    _ctrl = VideoController(_player);
-    _initPlayer();
-  }
+  void initState() { super.initState(); _initPlayer(); }
 
   Future<void> _initPlayer() async {
     final rawUrl = widget.channel.streamUrl;
@@ -34,20 +31,29 @@ class _State extends State<PlayerScreen> {
       }
     }
     headers.addAll(widget.channel.headers);
-    await _player.open(Media(url, httpHeaders: headers));
-    _player.play();
+    _ctrl?.dispose();
+    final ctrl = VideoPlayerController.networkUrl(Uri.parse(url), httpHeaders: headers);
+    await ctrl.initialize();
+    if (!mounted) return;
+    setState(() { _ctrl = ctrl; _buffering = false; });
+    ctrl.addListener(() {
+      if (!mounted) return;
+      if (ctrl.value.hasError && _reconnectAttempts < 5) {
+        _reconnectTimer?.cancel();
+        _reconnectTimer = Timer(const Duration(seconds: 3), () { _reconnectAttempts++; _initPlayer(); });
+      } else if (!ctrl.value.hasError) _reconnectAttempts = 0;
+    });
+    ctrl.play();
   }
 
   @override
-  void dispose() {
-    _player.dispose();
-    super.dispose();
-  }
+  void dispose() { _reconnectTimer?.cancel(); _ctrl?.dispose(); super.dispose(); }
 
   @override
   Widget build(BuildContext context) => Scaffold(
     backgroundColor: Colors.black,
-    body: GestureDetector(
-      onTap: () => Navigator.pop(context),
-      child: Center(child: Video(controller: _ctrl))));
+    body: _buffering || _ctrl == null
+      ? const Center(child: CircularProgressIndicator(color: AppTheme.accentCyan))
+      : GestureDetector(onTap: () => Navigator.pop(context),
+          child: SizedBox.expand(child: VideoPlayer(_ctrl!))));
 }
